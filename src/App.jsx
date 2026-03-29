f5cc6f92a3059fd5bdd6925758eac5402f859333
taposi barsa <taposibarsa@gmail.com>
2026-03-30 01:20:23 +0600

    i initialized git on my project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h2 className='text-red-600 text-center mt-7 text-3xl font-bold'> Hello world</h2>
+    </>
+  )
+}
+
+export default App
